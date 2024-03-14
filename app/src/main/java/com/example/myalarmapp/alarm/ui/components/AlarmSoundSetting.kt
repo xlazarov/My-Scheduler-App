3a20ad6df8c50548709165d702dd3e2029ff5790
Keťa <03m.lazarova@gmail.com>
2024-03-14 15:25:14 +0100

    imports cleanup

--- app/src/main/java/com/example/myalarmapp/alarm/ui/components/AlarmSoundSetting.kt
+++ app/src/main/java/com/example/myalarmapp/alarm/ui/components/AlarmSoundSetting.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.twotone.MusicNote
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
